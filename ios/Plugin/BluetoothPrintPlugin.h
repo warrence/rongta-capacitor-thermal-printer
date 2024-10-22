1aeeb78c5a886c940c69477f55512718fedf06e7
Malik12tree <82341209+Malik12tree@users.noreply.github.com>
2024-10-22 22:18:15 +0300

    feat(ios): use builtin bridging header

--- ios/Plugin/BluetoothPrintPlugin.h
+++ ios/Plugin/BluetoothPrintPlugin.h
@@ -8,3 +8,47 @@ FOUNDATION_EXPORT const unsigned char PluginVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Plugin/PublicHeader.h>
 
+
+#import <Plugin/SDK/include/RTPrinterSDK/BarcodeSetting.h>
+#import <Plugin/SDK/include/RTPrinterSDK/BitmapSetting.h>
+#import <Plugin/SDK/include/RTPrinterSDK/BlueToothFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/CPCLCmd.h>
+#import <Plugin/SDK/include/RTPrinterSDK/CPCLFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/Cmd.h>
+#import <Plugin/SDK/include/RTPrinterSDK/CmdFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/CommonSetting.h>
+#import <Plugin/SDK/include/RTPrinterSDK/ESCCmd.h>
+#import <Plugin/SDK/include/RTPrinterSDK/ESCFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/EnumTypeDef.h>
+#import <Plugin/SDK/include/RTPrinterSDK/EscpCmd.h>
+#import <Plugin/SDK/include/RTPrinterSDK/EscpFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/Header.h>
+#import <Plugin/SDK/include/RTPrinterSDK/LabelCommonSetting.h>
+#import <Plugin/SDK/include/RTPrinterSDK/LabelPrinter.h>
+#import <Plugin/SDK/include/RTPrinterSDK/LabelPrinterFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/NetworkPrinter.h>
+#import <Plugin/SDK/include/RTPrinterSDK/ObserverObj.h>
+#import <Plugin/SDK/include/RTPrinterSDK/PIFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/PinCmd.h>
+#import <Plugin/SDK/include/RTPrinterSDK/PinCmdFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/PinPrinter.h>
+#import <Plugin/SDK/include/RTPrinterSDK/PinPrinterFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/PlistUtils.h>
+#import <Plugin/SDK/include/RTPrinterSDK/Printer.h>
+#import <Plugin/SDK/include/RTPrinterSDK/PrinterFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/PrinterInterface.h>
+#import <Plugin/SDK/include/RTPrinterSDK/PrinterStatusObj.h>
+#import <Plugin/SDK/include/RTPrinterSDK/RTBlueToothPI.h>
+#import <Plugin/SDK/include/RTPrinterSDK/RTDeviceinfo.h>
+#import <Plugin/SDK/include/RTPrinterSDK/SafeMutableArray.h>
+#import <Plugin/SDK/include/RTPrinterSDK/TSCCmd.h>
+#import <Plugin/SDK/include/RTPrinterSDK/TSCFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/TextSetting.h>
+#import <Plugin/SDK/include/RTPrinterSDK/ThermalPrinter.h>
+#import <Plugin/SDK/include/RTPrinterSDK/ThermalPrinterFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/WIFIFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/WifiInterface.h>
+#import <Plugin/SDK/include/RTPrinterSDK/XsimCmd.h>
+#import <Plugin/SDK/include/RTPrinterSDK/XsimFactory.h>
+#import <Plugin/SDK/include/RTPrinterSDK/ZPLCmd.h>
+#import <Plugin/SDK/include/RTPrinterSDK/ZPLFactory.h>
